83bf411a9d9d9402e478dd0cd7c45a296bd1e06f
lisnger <bett.unger@web.de>
2018-09-18 17:53:25 +0200

    made game to function added typo checks functions still needs test

--- bikovIkarov.py
+++ bikovIkarov.py
@@ -53,32 +53,49 @@ def compare_numbers(n_to_guess, guess):
             same -= 1
     return same, correct
 
+def human_guess_number():
+    #TODO write test function
+    '''
+    This is the actual game bikov and karov, where a human has to guess a unique 6 digit number (0-9) in the right order.
+    When this function os called it starts an interactive mode and instructions are printed on the screen. 
+    '''
+    print "Guess the number (6 unique digits between 0-9)"
+    bikov = 0
+    count = 0
+    while bikov != 6:
+        count += 1
+        guess_int = raw_input()
+        while len(guess_int) < 6 or len(guess_int) > 6 or str.isdigit(guess_int) == False:
+            if len(guess_int) < 6 or len(guess_int) > 6:
+                print "The number has to have exactly 6 digits. Try again."
+                guess_int = raw_input()
+            if str.isdigit(guess_int) == False:
+                print "It has to be a number, no other characters. Try again."
+                guess_int = raw_input()
+        guess = map(int, guess_int)
+        #guess = [int(x) for x in str(guess_int)] # list comprehansion 
+        karov, bikov = compare_numbers(n_to_guess, guess)
+        print "%i karov, %i bikov"%(karov, bikov)
+        print "Guess again!"
+    print "Congratulations! You guessed the number in %i tries."%(count)
+
 
 if __name__=="__main__":
 
     n_to_guess = unique_random_number_list()
     print n_to_guess #TODO delete when done testing
     
-    print "How do you want to play?"
-    print "You guess the number provided by the computer, press: a"
-    print "You want the computer to guess your 6 digit number (must be unique between 0-9), press: b"
-    print "Do you want to play interactive? Press: c"
-    print "NOTE: only option a works right now, you can not choose the other two, haha"
-    option = raw_input()
+    #TODO, remove when other options are available
+    #print "How do you want to play?"
+    #print "You guess the number provided by the computer, press: a"
+    #print "You want the computer to guess your 6 digit number (must be unique between 0-9), press: b"
+    #print "Do you want to play interactive? Press: c"
+    #print "NOTE: only option a works right now, you can not choose the other two, haha"
+    #option = raw_input()
+    option = 'a'
 
     if option == 'a':
-        print "Guess the number (6 unique digits between 0-9)"
-        bikov = 0
-        count = 0
-        while bikov != 6:
-            count += 1
-            print "Guess again!"
-            guess_int = raw_input()
-            guess = map(int, str(guess_int))
-            #guess = [int(x) for x in str(guess_int)] # list comprehansion 
-            karov, bikov = compare_numbers(n_to_guess, guess)
-            print "%i karov, %i bikov"%(karov, bikov)
-        print "Congratulations! You guessed the number in %i tries."%(count)
+        human_guess_number()
 
     elif option == 'b':
         print "The computer will try to guess your unique 6 digit number."
